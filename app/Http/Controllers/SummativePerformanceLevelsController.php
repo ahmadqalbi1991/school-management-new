85701991a21cb0e2ac9f9917e3825c33971b7360
ahmadqalbi1991 <72644716+ahmadqalbi1991@users.noreply.github.com>
2023-07-26 11:45:15 -0700

    asdsad

--- app/Http/Controllers/SummativePerformanceLevelsController.php
+++ app/Http/Controllers/SummativePerformanceLevelsController.php
@@ -45,8 +45,7 @@ class SummativePerformanceLevelsController extends Controller
      */
     public function getList()
     {
-        $admins = getSchoolAdmins(Auth::user()->school_id);
-        dd($admins, Auth::user());
+        $admins = getSchoolAdmins();
         $data = SummativePerformnceLevel::when(in_array(Auth::user()->role, ['admin', 'teacher']), function ($q) use ($admins) {
             return $q->whereIn('created_by', $admins);
         })->latest()->get();
