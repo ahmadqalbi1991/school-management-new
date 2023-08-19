8963763859718cf4d863b4ebd213cc629a0c2c3a
ahmadqalbi1991 <72644716+ahmadqalbi1991@users.noreply.github.com>
2023-08-19 12:12:54 -0700

    fixing

--- app/Http/Controllers/FormativeAssessmentController.php
+++ app/Http/Controllers/FormativeAssessmentController.php
@@ -392,6 +392,8 @@ class FormativeAssessmentController extends Controller
             $learner = $data['learner'];
             $term = $data['term'];
 
+            return view('pdfs.result')->with($data);
+
             $pdf = PDF::loadView('pdfs.result', $data);
             if ($send_email) {
                 $content = $pdf->output();
